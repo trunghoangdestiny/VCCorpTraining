f4e79a6c401878737ebd9ded64ed7ad98081a800
trunghoangdestiny <trunghoang.destiny@gmail.com>
2020-09-27 23:44:20 +0700

    Edited code after test.

---
+++ src/updated_code/Week1Bai1.java
@@ -0,0 +1,37 @@
+package updated_code;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Week1Bai1 {
+    public static void main(String[] args) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        Set<Integer> setTemp = IntStream.rangeClosed(1, 10000).boxed().collect(Collectors.toSet());
+        set1.addAll(setTemp);
+        set2.addAll(setTemp);
+
+        Random random = new Random();
+        set1.addAll(random
+                .ints(10001, Integer.MAX_VALUE)
+                .distinct()
+                .limit(190000)
+                .boxed()
+                .collect(Collectors.toSet()));
+        set2.addAll(random
+                .ints()
+                .distinct()
+                .limit(190000)
+                .boxed()
+                .collect(Collectors.toSet()));
+
+        Set<Integer> intersection = new HashSet<>(set1);
+        intersection.retainAll(set2);
+        Set<Integer> union = new HashSet<>(set1);
+        union.addAll(set2);
+        System.out.printf("Intersection: %d, Union: %d", intersection.size(), union.size());
+    }
+}
--- src/week1/bai3/FileReaderCallableMain.java
+++ src/week1/bai3/FileReaderCallableMain.java
@@ -1,9 +1,6 @@
 package week1.bai3;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -12,8 +9,8 @@ import java.util.concurrent.Future;
 import java.util.stream.Stream;
 
 public class FileReaderCallableMain {
-    public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
-        ExecutorService executorService = Executors.newFixedThreadPool(6); //create a thread pool with maximum 6 threads
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(6); //create a thread pool with maximum 5 sub threads
         List<FileReaderCallable> fileReaderCallables = new ArrayList<>(); //create a list of file in input folder
         List<Future<LinkedHashMap<String, Integer>>> futureList = new ArrayList<>(); //create a list of future to get result
         LinkedHashMap<String, Integer> finalResult = new LinkedHashMap<>();
@@ -46,41 +43,42 @@ public class FileReaderCallableMain {
             });
         }
 
-        FileWriter fileOutput = new FileWriter("/home/hoangquoctrung/IdeaProjects/File/output.txt");
-        FileWriter fileMostOccurrence = new FileWriter("/home/hoangquoctrung/IdeaProjects/File/the_most_occurrence.txt");
-        BufferedWriter bufferedWriter = new BufferedWriter(fileOutput);
-        BufferedWriter bufferedWriter1 = new BufferedWriter(fileMostOccurrence);
+        try (
+                FileWriter fileOutput = new FileWriter("/home/hoangquoctrung/IdeaProjects/File/output.txt");
+                FileWriter fileMostOccurrence = new FileWriter("/home/hoangquoctrung/IdeaProjects/File/the_most_occurrence.txt");
+                BufferedWriter bufferedWriter = new BufferedWriter(fileOutput);
+                BufferedWriter bufferedWriter1 = new BufferedWriter(fileMostOccurrence)
+        ) {
 
-        //render data to the output file
-        finalResult.forEach((key, value) -> {
-            try {
-                bufferedWriter.write(key + ": " + value);
-                bufferedWriter.newLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
+            //render data to the output file
+            finalResult.forEach((key, value) -> {
+                try {
+                    bufferedWriter.write(key + ": " + value);
+                    bufferedWriter.newLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
 
-        //render data to the the_most_occurrence file
-        Stream<Map.Entry<String, Integer>> sortedResult = finalResult
-                .entrySet()
-                .stream()
-                .sorted((e1, e2)->Integer.compare(e2.getValue(), e1.getValue()))
-                .limit(10);
-        bufferedWriter1.write("Top 10 the most occurrence words:\n");
-        sortedResult.forEach(element->{
-            try {
-                bufferedWriter1.write(element.getKey() + ": " + element.getValue());
-                bufferedWriter1.newLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
+            //render data to the the_most_occurrence file
+            Stream<Map.Entry<String, Integer>> sortedResult = finalResult
+                    .entrySet()
+                    .stream()
+                    .sorted((e1, e2) -> Integer.compare(e2.getValue(), e1.getValue()))
+                    .limit(10);
+            bufferedWriter1.write("Top 10 the most occurrence words:\n");
+            sortedResult.forEach(element -> {
 
-        bufferedWriter.close();
-        bufferedWriter1.close();
-        fileOutput.close();
-        fileMostOccurrence.close();
+                try {
+                    bufferedWriter1.write(element.getKey() + ": " + element.getValue());
+                    bufferedWriter1.newLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         executorService.shutdown();
     }
 }
