83a429de41bd4cd0fae93504be734aced0f04c7b
trunghoangdestiny <trunghoang.destiny@gmail.com>
2020-09-28 22:08:49 +0700

    Edited code2.

--- src/week1/bai4/PointGenerator.java
+++ src/week1/bai4/PointGenerator.java
@@ -1,8 +1,6 @@
 package week1.bai4;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 //sinh 30000 diem phan biet, dung set
 
 public class PointGenerator {
@@ -10,33 +8,30 @@ public class PointGenerator {
         Point A = new Point(800, 800, 0); //8000 points distance to A not over 400 units
         Point B = new Point(4000, 800, 0); //10000 points distance to B not over 500 units
         Point C = new Point(2400, 2400, 0); //12000 points distance to C not over 600 units
-        List<Point> pointList = new ArrayList<>();
-        pointList.addAll(generatePoint(8000, A, 400));
-        pointList.addAll(generatePoint(10000, B, 500));
-        pointList.addAll(generatePoint(12000, C, 600));
-        System.out.println(pointList.size());
-        pointList.forEach(point -> {
-            if (point.getDistanceToX() >= 599)
-                System.out.println(point);
-        });
+        Set<Point> pointSet = new HashSet<>();
+        pointSet.addAll(generatePoint(8000, A, 400));
+        pointSet.addAll(generatePoint(10000, B, 500));
+        pointSet.addAll(generatePoint(12000, C, 600));
+        System.out.println(pointSet.size());
+        System.out.println(pointSet);
     }
 
-    public static boolean check(List<Point> pointList, int numberPoint) {
-        return pointList.size() >= numberPoint;
+    public static boolean check(Set<Point> pointSet, int numberPoint) {
+        return pointSet.size() >= numberPoint;
     }
 
-    public static List<Point> generatePoint(int numberPoint, Point X, int maxDistance) {
-        List<Point> pointList = new ArrayList<>();
+    public static Set<Point> generatePoint(int numberPoint, Point X, int maxDistance) {
+        Set<Point> pointSet = new HashSet<>();
         Random random = new Random();
         while (true) {
-            if (check(pointList, numberPoint))
+            if (check(pointSet, numberPoint))
                 break;
             int x = random.nextInt(2 * maxDistance) + X.getX() - maxDistance;
             int y = random.nextInt(2 * maxDistance) + X.getY() - maxDistance;
             double distance = Math.sqrt(Math.pow(x - X.getX(), 2) + Math.pow(y - X.getY(), 2));
             if (distance <= maxDistance)
-                pointList.add(new Point(x, y, distance));
+                pointSet.add(new Point(x, y, distance));
         }
-        return pointList;
+        return pointSet;
     }
 }
