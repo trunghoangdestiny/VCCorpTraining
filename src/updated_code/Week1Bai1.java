3100d5571f2d43f9edaa2c350fddd204f4f844bb
trunghoangdestiny <trunghoang.destiny@gmail.com>
2020-09-29 11:08:03 +0700

    Edited code3.

--- src/updated_code/Week1Bai1.java
+++ src/updated_code/Week1Bai1.java
@@ -1,5 +1,7 @@
 package updated_code;
 
+import week1.bai1.TimeCounter;
+
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
@@ -8,30 +10,34 @@ import java.util.stream.IntStream;
 
 public class Week1Bai1 {
     public static void main(String[] args) {
-        Set<Integer> set1 = new HashSet<>();
-        Set<Integer> set2 = new HashSet<>();
-        Set<Integer> setTemp = IntStream.rangeClosed(1, 10000).boxed().collect(Collectors.toSet());
-        set1.addAll(setTemp);
-        set2.addAll(setTemp);
+        Runnable runnable = () -> {
+            Set<Integer> set1 = new HashSet<>();
+            Set<Integer> set2 = new HashSet<>();
+            Set<Integer> setTemp = IntStream.rangeClosed(1, 10000).boxed().collect(Collectors.toSet());
+            set1.addAll(setTemp);
+            set2.addAll(setTemp);
 
-        Random random = new Random();
-        set1.addAll(random
-                .ints(10001, Integer.MAX_VALUE)
-                .distinct()
-                .limit(190000)
-                .boxed()
-                .collect(Collectors.toSet()));
-        set2.addAll(random
-                .ints()
-                .distinct()
-                .limit(190000)
-                .boxed()
-                .collect(Collectors.toSet()));
+            Random random = new Random();
+            set1.addAll(random
+                    .ints(10001, Integer.MAX_VALUE)
+                    .distinct()
+                    .limit(190000)
+                    .boxed()
+                    .collect(Collectors.toSet()));
+            set2.addAll(random
+                    .ints()
+                    .distinct()
+                    .limit(190000)
+                    .boxed()
+                    .collect(Collectors.toSet()));
 
-        Set<Integer> intersection = new HashSet<>(set1);
-        intersection.retainAll(set2);
-        Set<Integer> union = new HashSet<>(set1);
-        union.addAll(set2);
-        System.out.printf("Intersection: %d, Union: %d", intersection.size(), union.size());
+            Set<Integer> intersection = new HashSet<>(set1);
+            intersection.retainAll(set2);
+            Set<Integer> union = new HashSet<>(set1);
+            union.addAll(set2);
+            System.out.printf("Intersection: %d, Union: %d", intersection.size(), union.size());
+        };
+        TimeCounter timeCounter = new TimeCounter(runnable);
+        timeCounter.timeCounter();
     }
 }
--- src/updated_code/Week1Bai2.java
+++ src/updated_code/Week1Bai2.java
@@ -1,4 +1,39 @@
 package updated_code;
 
+import java.sql.*;
+
 public class Week1Bai2 {
+    private static String URL = "jdbc:mysql://localhost:3306/mydatabase?useSSL=false";
+    private static String USER = "root";
+    private static String PASS = "";
+
+    public static void main(String[] args) {
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASS)) {
+            System.out.println("Connected successfully.");
+            String query = "select *\n" +
+                    "from (\n" +
+                    "         select country.code,\n" +
+                    "                country.name as CountryName,\n" +
+                    "                city.name       CityName,\n" +
+                    "                city.population,\n" +
+                    "                ROW_NUMBER() over (\n" +
+                    "                    partition by country.code\n" +
+                    "                    order by city.population desc\n" +
+                    "                    )        as row_num\n" +
+                    "         from city,\n" +
+                    "              country\n" +
+                    "         where codeCountry = country.code\n" +
+                    "     ) rank_city_of_each_country\n" +
+                    "where row_num = 1;";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(1) + ", " + resultSet.getString(2) + ": " + resultSet.getString(3)
+                        + ", " + resultSet.getInt(4));
+            }
+        } catch (SQLException e) {
+            System.out.println("Connected failed.");
+            e.printStackTrace();
+        }
+    }
 }
